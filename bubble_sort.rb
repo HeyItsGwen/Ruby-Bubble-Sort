efb3cdc43782d39164a88deff764f41f81f1fa80
Gwen <itsgwenhey@gmail.com>
2019-11-26 13:28:58 -0600

    rubocop fix
    fix rubocop errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,25 +1,25 @@
 # frozen_string_literal: true
 
 def bubble_sort(arr)
-  loop {
+  loop do
     swap_count = 0
 
-    (0...arr.length).each { |i|
+    (0...arr.length).each do |i|
       next_num = i + 1
 
-      unless arr[next_num].nil?
-        if arr[i] > arr[next_num]
-          temp = arr[i]
-          arr[i] = arr[next_num]
-          arr[next_num] = temp
+      next if arr[next_num].nil?
 
-          swap_count += 1
-        end
-      end
-    }
+      next unless arr[i] > arr[next_num]
+
+      temp = arr[i]
+      arr[i] = arr[next_num]
+      arr[next_num] = temp
+
+      swap_count += 1
+    end
 
     break if swap_count.zero?
-  }
+  end
   arr
 end
 
@@ -31,15 +31,15 @@ def bubble_sort_by(arr)
       next_num = i + 1
       next if arr[next_num].nil?
 
-        comparison = yield arr[i], arr[next_num]
+      comparison = yield arr[i], arr[next_num]
 
       next unless comparison.positive?
 
-        temp = arr[i]
-        arr[i] = arr[next_num]
-        arr[next_num] = temp
+      temp = arr[i]
+      arr[i] = arr[next_num]
+      arr[next_num] = temp
 
-        swap_count += 1
+      swap_count += 1
     end
     break if swap_count.zero?
   end
@@ -49,5 +49,5 @@ end
 array = [1, 4, 3, 1]
 
 unsorted = %w[hi hello hey]
-puts bubble_sort_by(unsorted) { |left, right| right.length-left.length  }
+puts bubble_sort_by(unsorted) { |left, right| right.length - left.length }
 puts bubble_sort(array)
