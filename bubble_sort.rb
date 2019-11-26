174e5d4a4e2b4869ed23ce5704d9b62303655a10
Gwen <itsgwenhey@gmail.com>
2019-11-26 11:41:49 -0600

    fix bubble sort by
    fix bubble sort by to work without the <=> per review instructions

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -6,7 +6,11 @@ def bubble_sort_by(arr)
 
     (0...arr.length).each do |i|
       next_num = i + 1
-      next unless yield(arr[i], arr[next_num]) == 1
+      next if arr[next_num].nil?
+
+      comparison = yield arr[i], arr[next_num]
+
+      next unless comparison.positive?
 
       temp = arr[i]
       arr[i] = arr[next_num]
@@ -14,13 +18,18 @@ def bubble_sort_by(arr)
 
       swap_count += 1
     end
-
     break if swap_count.zero?
   end
-
   arr
 end
 
 def bubble_sort(arr)
   bubble_sort_by(arr) { |x, y| x <=> y }
 end
+
+array = [1, 3, 2]
+
+puts bubble_sort_by(array) { |x, y| y <=> x }
+
+unsorted = %w[hi hello hey]
+puts bubble_sort_by(unsorted) { |left, right| left.length - right.length }
